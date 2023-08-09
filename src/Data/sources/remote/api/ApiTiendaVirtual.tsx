9f556552e58ead8d21326493ea8c20cd38af925d
theacademydvr <theacademy.dvr@gmail.com>
2023-08-08 23:56:24 -0500

    CapturaySelecionaImagen-TV

--- src/Data/sources/remote/api/ApiTiendaVirtual.tsx
+++ src/Data/sources/remote/api/ApiTiendaVirtual.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiTiendaVirtual = axios.create({
-    baseURL: "http://192.168.1.12:3000/api-tsoftware",
+    baseURL: "http://192.168.1.17:3000/api-tsoftware",
     headers: {
         "Content-Type": "application/json",
     },
